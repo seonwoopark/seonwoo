ac81ba9921d6436a30943f6ec4a94ad96c3f1a71
park <tjsdn3823@naver.com>
2019-10-29 17:58:31 +0900

    3

--- ok.js
+++ ok.js
@@ -1,8 +1,18 @@
-var http = require('http');
-
-http.createServer(function(request, response){
-	response.writeHead(200, {'Content-Type':'text/html'});
-	response.end('<h1>Test-File-2-jing</h1>');
+require('http').createServer(function(request, response){
+	if(request.url == '/'){
+		response.write('<!DOCTYPE html>');
+		response.write('<html>');
+		response.write('<head>');
+		response.write('	<title>Forever</title>');
+		response.write('</head>');
+		response.write('<body>');
+		response.write('	<h1>Forever</h1>');
+		response.write('</body>');
+		response.write('</html>');
+		response.end();
+	}else{
+		error.error.error();
+	}
 }).listen(52273, function(){
 	console.log('Server Running at http://127.0.0.1:52273');
 });
