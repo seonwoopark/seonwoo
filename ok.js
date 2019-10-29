cafb48413452903d3441c7250c625a6175783823
park <tjsdn3823@naver.com>
2019-10-29 17:57:16 +0900

    2

--- ok.js
+++ ok.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function(request, response){
 	response.writeHead(200, {'Content-Type':'text/html'});
-	response.end('<h1>Test-File-1</h1>');
+	response.end('<h1>Test-File-2-jing</h1>');
 }).listen(52273, function(){
 	console.log('Server Running at http://127.0.0.1:52273');
 });
