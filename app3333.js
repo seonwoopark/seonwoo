5ac1aac89ff8007c5047f44372e8768d6c330002
park <tjsdn3823@naver.com>
2019-11-12 18:51:51 +0900

    8-26~29

--- app3333.js
+++ app3333.js
@@ -1,20 +1,48 @@
+var fs = require('fs');
 var express = require('express');
+var multipart = require('connect-multiparty');
+
 var app = express();
 
-var routerA = express.Router();
-var routerB = express.Router();
+app.use(multipart({uploadDir: __dirname + '/multipart'}));
 
-routerA.get('/index', function(request, response) {
-	response.send('<h1>Index Page</h1>');
+app.get('/', function(request, response) {
+	fs.readFile('insert.html', function(error, data) {
+		response.send(data.toString());
+	});
 });
 
-routerB.get('/index', function(request, response) {
-	response.send('<h1>Index Page</h1>');
-});
+app.post('/', function (request, response) {
+	   var comment = request.body.comment;
+	   var imageFile = request.files.image;
 
-app.use('/a', routerA);
-app.use('/b', routerB);
+	if (imageFile && imageFile.size > 0) {
+	   var name = imageFile.name;
+	   var path = imageFile.path;
+	   var type = imageFile.type;
+
+	if(type.indexOf('image') != -1) {
+	   var outputPath = __dirname + '/multipart/' + Date.now() + '_' + name;
+	   fs.rename(path, outputPath, function (error) {
+	      response.redirect('/');
+	   });
+	}else{
+	   fs.unlink(path, function (error) {
+	      response.sendStatus(400);
+	   });
+	}
+	}else{
+	   response.sendStatus(404);
+	}
+	});
+
+app.post('/', function (request, response) {
+	console.log(request.body);
+	console.log(request.files);
+	
+	response.redirect('/');
+});
 
 app.listen(52273, function() {
-	console.log('Server Running');
+	console.log('Server');
 });
